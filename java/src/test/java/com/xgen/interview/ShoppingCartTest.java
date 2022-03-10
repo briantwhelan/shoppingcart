6a43cb044c12c6ca8a3831d842dfd8ef4c28e38b
briantwhelan <whelanb8@tcd.ie>
2022-03-10 16:27:39 +0000

    Update JUnit tests
    This commit adds new unit tests to test the new features as well as
    modifying existing JUnit tests to simplify them. A setUp() and
    tearDown() has been added to enable objects and variables to be created
    and destroyed in an organised manner. This refactoring will mean that
    maintaining and updating the tests going forward should be relatively
    straightforward.

--- java/src/test/java/com/xgen/interview/ShoppingCartTest.java
+++ java/src/test/java/com/xgen/interview/ShoppingCartTest.java
@@ -3,100 +3,126 @@ package com.xgen.interview;
 import com.xgen.interview.Pricer;
 import com.xgen.interview.ShoppingCart;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 public class ShoppingCartTest
 {
-    @Test
-    public void canAddAnItem()
+    private ShoppingCart shoppingCart;
+    private Item item;
+    private ByteArrayOutputStream myOut;
+    
+    @Before
+    public void setUp()
     {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("apple", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        shoppingCart = new ShoppingCart(new Pricer());
+        item = new Item("apple", 2, 100);
+        myOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(myOut));
 
-        sc.printReceipt();
-        assertEquals(String.format("apple - 1 - €1.00%nTotal: €1.00%n"), myOut.toString());
     }
 
-    @Test
-    public void canAddMoreThanOneItem()
+    @After
+    public void tearDown()
     {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("apple", 2);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-        assertEquals(String.format("apple - 2 - €2.00%nTotal: €2.00%n"), myOut.toString());
-    }
-
-    @Test
-    public void canAddDifferentItems()
-    {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("apple", 2);
-        sc.addItem("banana", 1);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-
-        String result = myOut.toString();
-
-        if(result.startsWith("apple"))
+        try
         {
-            assertEquals(String.format("apple - 2 - €2.00%nbanana - 1 - €2.00%nTotal: €4.00%n"), result);
+            myOut.close();  
         }
-        else
+        catch(IOException e)
         {
-            assertEquals(String.format("banana - 1 - €2.00%napple - 2 - €2.00%nTotal: €4.00%n"), result);
+            System.out.println("Error closing byte array output stream...");
         }
     }
-
-    @Test
-    public void doesntExplodeOnMysteryItem()
-    {
-        ShoppingCart sc = new ShoppingCart(new Pricer());
-
-        sc.addItem("crisps", 2);
-
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(myOut));
-
-        sc.printReceipt();
-        assertEquals(String.format("crisps - 2 - €0.00%nTotal: €0.00%n"), myOut.toString());
-    }
-
-    @Test
-    public void canRetrieveItemFields()
-    {
-        Item item = new Item("apple", 2, 100);
-        assertEquals("apple", item.getName());
-        assertEquals(2, item.getQuantity());
-        assertEquals(100, item.getPrice());
-    }
-
+ 
     @Test
     public void canAdjustItemQuantity()
     {
-        Item item = new Item("apple", 2, 100);
         item.adjustQuantity(-3);
         assertEquals("Testing invalid quantity adjustment...", 2, item.getQuantity());
         item.adjustQuantity(5);
         assertEquals("Testing valid positive quantity adjustment...", 7, item.getQuantity());
         item.adjustQuantity(-2);
         assertEquals("Testing valid negative quantity adjustment...", 5, item.getQuantity());
+    }
+
+    @Test
+    public void canRetrieveItemFields()
+    {
+        assertEquals("Testing retrieving the item type...", "apple", item.getType());
+        assertEquals("Testing retrieving the item quantity type...", 2, item.getQuantity());
+        assertEquals("Testing retrieving the item price (in Euro)...", 2.00, item.getTotalPriceInEuro(), 0.0);
+    }
+
+    @Test
+    public void canPrintItemWithTypeFirst()
+    {
+        item.printTypeFirst();
+        assertEquals("Testing printing item with type first...", "apple - 2 - €2.00", (myOut.toString()).trim());
+    }
+    
+    @Test
+    public void canPrintItemWithQuantityFirst()
+    {
+        item.printQuantityFirst();
+        assertEquals("Testing printing item with quantity first...", "2 - apple - €2.00", (myOut.toString()).trim());
+    }
+    
+    @Test
+    public void canPrintItemWithPriceFirst()
+    {
+        item.printPriceFirst();
+        assertEquals("Testing printing item with price first...", "€2.00 - apple - 2", (myOut.toString()).trim());
+    }
+
+    @Test
+    public void canAddAnItem()
+    {
+        shoppingCart.addItem("apple", 1);    
+        shoppingCart.printReceipt();
+        assertEquals(String.format("apple - 1 - €1.00%nTotal: €1.00%n"), myOut.toString());
+    }
+
+    @Test
+    public void canAddMoreThanOneItem()
+    {
+        shoppingCart.addItem("apple", 2);
+        shoppingCart.printReceipt();
+        assertEquals(String.format("apple - 2 - €2.00%nTotal: €2.00%n"), myOut.toString());
+    }
+
+    @Test
+    public void canAddDifferentItems()
+    {
+        shoppingCart.addItem("apple", 2);
+        shoppingCart.addItem("banana", 1);
+        shoppingCart.printReceipt();
+
+        assertEquals(String.format("apple - 2 - €2.00%nbanana - 1 - €2.00%nTotal: €4.00%n"), myOut.toString());
+    }
+    
+    @Test
+    public void canAddTheSameItems()
+    {
+        shoppingCart.addItem("apple", 2);
+        shoppingCart.addItem("apple", 1);
+        shoppingCart.printReceipt();
+
+        assertEquals(String.format("apple - 3 - €3.00%nTotal: €3.00%n"), myOut.toString());
+    }
+
+    @Test
+    public void doesntExplodeOnMysteryItem()
+    {
+        shoppingCart.addItem("crisps", 2);
+        shoppingCart.printReceipt();
+        assertEquals(String.format("crisps - 2 - €0.00%nTotal: €0.00%n"), myOut.toString());
     }
 }
 
